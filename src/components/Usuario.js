3b5f15fc06b4ea3d65eaccb4010f7104f1ba619f
Eduardo Souza <edcampossouza@gmail.com>
2022-11-15 19:55:22 -0300

    Change username

--- src/components/Usuario.js
+++ src/components/Usuario.js
@@ -1,10 +1,22 @@
+import { useState } from "react";
+
 export default function Usuario({ user }) {
+  const [realname, setRealName] = useState(user.realname);
+  function changeRealName() {
+    const newName = prompt("Digite o novo nome...");
+    if (!newName) {
+      alert("Nome invalido!");
+    } else setRealName(newName);
+  }
   return (
     <div className="perfil-usuario">
       <img src={user.image} />
       <div>
         <div className="nomeperfil">{user.username}</div>
-        <div className="nomereal">{user.realname}</div>
+        <div className="nomereal">
+          {realname}{" "}
+          <ion-icon onClick={changeRealName} name="pencil-outline"></ion-icon>
+        </div>
       </div>
     </div>
   );
