5ead6bddf6f3eaaa0121676021b1cb29f6528f7d
Eduardo Souza <edcampossouza@gmail.com>
2022-11-15 20:22:01 -0300

    added: change profile image functionality

--- src/components/Usuario.js
+++ src/components/Usuario.js
@@ -2,15 +2,22 @@ import { useState } from "react";
 
 export default function Usuario({ user }) {
   const [realname, setRealName] = useState(user.realname);
+  const [imgLink, setImgLink] = useState(user.image);
   function changeRealName() {
     const newName = prompt("Digite o novo nome...");
     if (!newName) {
-      alert("Nome invalido!");
+      alert("Nome vazio!");
     } else setRealName(newName);
+  }
+  function changeImgLink() {
+    const newLink = prompt("Digite o novo link...");
+    if (!newLink) {
+      alert("Link vazio!");
+    } else setImgLink(newLink);
   }
   return (
     <div className="perfil-usuario">
-      <img src={user.image} />
+      <img onClick={changeImgLink} src={imgLink} />
       <div>
         <div className="nomeperfil">{user.username}</div>
         <div className="nomereal">
